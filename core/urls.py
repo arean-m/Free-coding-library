41ea744b9da439f77887df38f72bc2e30250fcbc
arean-m <areanmorshedofc@gmail.com>
2026-06-24 14:53:32 +0800

    Fixed typo in core urls blueprint

--- core/urls.py
+++ core/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from resources import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),            
+    path('admin/', admin.site.urls),           
     path('', views.resource_home, name='home'),
 ]
