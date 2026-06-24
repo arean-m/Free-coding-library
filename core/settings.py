653b24e6ddba8012b8b1949cb68ab27655765915
arean-m <areanmorshedofc@gmail.com>
2026-06-24 16:06:55 +0800

    Add Render URL to trusted CSRF origins

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,15 @@ SECRET_KEY = 'django-insecure-c_#5iu*q#idw%3j8+_&&59-8jto-f4@#z@6az)hn)&6)nu@$zl
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [
+    'free-coding-library.onrender.com',
+    'localhost',
+    '127.0.0.1',
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://free-coding-library.onrender.com',
+]
 
 
 # Application definition
