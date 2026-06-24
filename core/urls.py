6366d41b614a8b6aa39e35735d25008e0b92412c
arean-m <areanmorshedofc@gmail.com>
2026-06-24 15:32:05 +0800

    Removed ghost function routing and pointed form to root

--- core/urls.py
+++ core/urls.py
@@ -5,5 +5,4 @@ from resources import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.resource_home, name='home'),
-    path('submit/', views.submit_resource, name='submit_resource'),
 ]
